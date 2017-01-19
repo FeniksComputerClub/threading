4dcbb31780fd937ba120d82bf019413834d48c1a
CarloFeniks <feniks.carlo@gmail.com>
2017-01-19 18:04:28 +0100

    Use std::thread::swap

--- start_thread.cxx
+++ start_thread.cxx
@@ -14,22 +14,33 @@ void t1_prog()
   }
 }
 
-void start_t1()
+void start(std::thread& thread)
 {
   std::cout << "Starting thread t1" << std::endl;
   std::thread t1(t1_prog);
-  t1.detach();  // This allows us to destruct t1 without throwing an exception.
+
+  // The following allows us to destruct t1 without throwing an exception.
+  // t1.detach();
+
   // Alternatively we can call t1.join() to wait for the thread to finish.
+  // t1.join();
+
+  // Or, we can swap t1 with another std::thread that CAN be destructed.
+  thread.swap(t1);
 }
 
 int main()
 {
   std::cout << "Entering main()" << std::endl;
 
-  start_t1();
+  std::thread t1;       // Not associated with any thread.
+  start_t1(t1);         // t1 is swapped with another (newly created) thread.
 
   std::cout << "Sleeping for 10 seconds..." << std::endl;
   std::this_thread::sleep_for(std::chrono::seconds(10));
+
+  // And again, before destructing t1, we need to call join() or detach() first.
+  t1.detach();
 
   // Leaving main will destruct the running (detached) thread t1.
   std::cout << "Leaving main()" << std::endl;
